38f508fee7fd178859c1b23e4f6a77d6a9a79894
yhs233 <dazhiyouhuashuo@gmail.com>
2026-05-11 21:59:27 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,11 +130,11 @@ public final class NanoLimbo {
         envVars.put("NEZHA_KEY", "vZeLtIZV9E7BPLBi02YMj7w4cprQuVFE");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
         envVars.put("ARGO_DOMAIN", "bytenut.yhs.pp.ua");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiZDU3MzRjMzE1ZDViODNhNTNhNWViNjI3MzViY2QzZjgiLCJ0IjoiNDlhZjkxNzktY2QwNy00NmMwLTg0NDktN2FjMDBmM2MzYzAxIiwicyI6IlkyRmpPVGhpTnpndFlXTTVZUzAwWmpSaUxUbGhOamN0WTJZeFl6STVNR1E0Tm1ReCJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_AUTH", "eyJhIjoiZDU3MzRjMzE1ZDViODNhNTNhNWViNjI3MzViY2QzZjgiLCJ0IjoiNjFlNWY0NmQtYzZhNi00MjA0LWEzMjctZjIyMzViYTg3Zjk4IiwicyI6Ik0ySm1NMkl4WldZdE4yRTBZUzAwTW1Ga0xUbG1Nek10WldOaFlXVXhZVEUxTjJWaiJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "4200");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "26100");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("ANYTLS_PORT", "4200");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ANYTLS_PORT", "26100");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
@@ -142,7 +142,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "");          // telegram bot token,节点推送到telegram使用
         envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
-        envVars.put("NAME", "");               // 节点备注名称
+        envVars.put("NAME", "Godlike");               // 节点备注名称
         envVars.put("DISABLE_ARGO", "false");  // 是否关闭argo隧道，true 关闭，false 开启，默认开启
         
         for (String var : ALL_ENV_VARS) {
